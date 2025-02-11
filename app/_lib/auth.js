145572fe788a57a8057021bf110376a71cdcb256
Abdi036 <abdikumela036@gmail.com>
2025-02-10 23:24:29 -1200

    add guest user handling in authentication flow; create guest on sign-in and attach guest ID to session

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,11 +1,34 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { createGuest, getGuest } from "./data-service";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const exsistingGuest = await getGuest(user.email);
+
+        if (!exsistingGuest) {
+          createGuest({
+            email: user.email,
+            fullname: user.name,
+          });
+        }
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+
+    async session({ session, user }) {
+      const guest = await getGuest(user.email);
+      session.user.guestID = guest.id;
+      return session;
     },
   },
   pages: {
